c009b9491354c44caafdf7cd7ca8b482a2104c86
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-03-01 01:01:42 -0800

    Initial Commit

---
+++ PhotoEditorApp/PhotoEditorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorAppApp.swift
+//  PhotoEditorApp
+//
+//  Created by Sothesom on 11/12/1402.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
